0f31f0a1b50c19589a6e8b6259620518f23b2d18
Vlad Paulechka <vlad9.ru@gmail.com>
2022-10-13 12:24:25 +0300

    правки Новые клавиатуры

--- keyboards/user/about_us.py
+++ keyboards/user/about_us.py
@@ -1,14 +1,38 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
+# async def about_us_kb() -> InlineKeyboardMarkup:
+#     keyboard = InlineKeyboardMarkup(
+#             row_width=1
+#         )
+#     keyboard.add(*[
+#                 InlineKeyboardButton('Наш номер телефона', callback_data='phone_number'),
+#                 InlineKeyboardButton('Время работы', callback_data='work_time'),
+#                 InlineKeyboardButton('Адреса кафе', callback_data='adress'),
+#                 InlineKeyboardButton('Назад', callback_data='back')
+#             ])
+#     return keyboard
+
+
 async def about_us_kb() -> InlineKeyboardMarkup:
-    keyboard = InlineKeyboardMarkup(
-            row_width=1
-        )
-    keyboard.add(*[
-                InlineKeyboardButton('Наш номер телефона', callback_data='phone_number'),
-                InlineKeyboardButton('Время работы', callback_data='work_time'),
-                InlineKeyboardButton('Адреса кафе', callback_data='adress'),
-                InlineKeyboardButton('Назад', callback_data='back')
-            ])
-    return keyboard
+    """"
+    Описание что делает функция
+    :param Какие параметры принимает и для чего
+    :return Что будет на выходе
+    """
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text="Наш номер телефона", callback_data="phone_number")
+            ],
+            [
+                InlineKeyboardButton(text="Время работы", callback_data="work_time")
+            ],
+            [
+                InlineKeyboardButton(text="Адреса кафе", callback_data="adress")
+            ],
+            [
+                InlineKeyboardButton(text="Назад", callback_data="back")
+            ]
+        ]
+    )
--- keyboards/user/back.py
+++ keyboards/user/back.py
@@ -1,11 +1,25 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
+# async def back_kb(target: str) -> InlineKeyboardMarkup:
+#     keyboard = InlineKeyboardMarkup(
+#             row_width=1
+#         )
+#     keyboard.add(*[
+#                 InlineKeyboardButton('Назад', callback_data=target)
+#             ])
+#     return keyboard
+
+
 async def back_kb(target: str) -> InlineKeyboardMarkup:
-    keyboard = InlineKeyboardMarkup(
-            row_width=1
-        )
-    keyboard.add(*[
-                InlineKeyboardButton('Назад', callback_data=target)
-            ])
-    return keyboard
+    """"
+    Клавиатура назад
+    :return InlineKeyboardMarkup
+    """
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text="Назад", callback_data=target)
+            ]
+        ]
+    )
