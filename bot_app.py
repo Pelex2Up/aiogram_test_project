1654ace477f584dd82b235a8f50a2b04ebe20d90
Vlad Paulechka <vlad9.ru@gmail.com>
2022-10-14 11:20:54 +0300

    правки

--- bot_app.py
+++ bot_app.py
@@ -3,10 +3,17 @@ import datetime
 import asyncio
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
+from utils.set_bot_commands import set_default_commands
+
+
+async def on_startup(dp):
+    await set_default_commands(dp)
+
 
 if __name__ == '__main__':
     from aiogram.utils import executor
     from handlers import dp
+
     print(f'{datetime.datetime.now()} -- Test_cafe_bot: Начинаю работу.')
     dp.middleware.setup(LoggingMiddleware())
-    executor.start_polling(dp)
+    executor.start_polling(dp , on_startup=on_startup)
--- handlers/message/booking.py
+++ handlers/message/booking.py
@@ -32,6 +32,7 @@ async def l_name_booking(message: types.Message, state: FSMContext):
     await state.update_data(f_name=message.text)
     await BookingStates.next()
     await BookingStates.l_name.set()
+    await message.delete()
     await message.answer('Введите Вашу фамилию:')
 
 
@@ -40,11 +41,12 @@ async def date_booking(message: types.Message, state: FSMContext):
     await state.update_data(l_name=message.text)
     await BookingStates.next()
     await BookingStates.date.set()
+    await message.delete()
     await message.answer('Выберите дату:',
                          reply_markup=await SimpleCalendar().start_calendar())
 
 
-def is_valid_date(date: str) -> bool:
+async def is_valid_date(date: str) -> bool:
     today_date = datetime.now().strftime("%d/%m/%Y")
     today_date = today_date.split('/')
     date = date.split('/')
@@ -60,22 +62,22 @@ def is_valid_date(date: str) -> bool:
 
 @dp.callback_query_handler(simple_cal_callback.filter(), state=BookingStates.date)
 async def verify_selection(callback_query: CallbackQuery, callback_data: dict, state: FSMContext):
-    selected, date = await SimpleCalendar().process_selection(callback_query, callback_data['text'])
+    selected, date = await SimpleCalendar().process_selection(callback_query, callback_data)
     if selected:
-        await callback_query.message.answer(f'Бронирование на {date.strftime("%d/%m/%Y")}')
-    result_select_date = date.strftime("%d/%m/%Y")
-    if not is_valid_date(result_select_date):
-        await callback_query.message.answer(f'Выбрана некорректная дата. Проверьте правильность ввода.',
-                                            reply_markup=await SimpleCalendar().start_calendar())
-    else:
-        await state.update_data(date=result_select_date)
-        await BookingStates.next()
-        await BookingStates.time.set()
-        await callback_query.message.answer('На какое время хотите забронировать столик?\n'
-                                            'Время вводится в формате Часы:Минуты')
+        await callback_query.message.edit_text(f'Бронирование на {date.strftime("%d/%m/%Y")}')
+        result_select_date = date.strftime("%d/%m/%Y")
+        if not await is_valid_date(result_select_date):
+            await callback_query.message.answer(f'Выбрана некорректная дата. Проверьте правильность ввода.',
+                                                reply_markup=await SimpleCalendar().start_calendar())
+        else:
+            await state.update_data(date=result_select_date)
+            await BookingStates.next()
+            await BookingStates.time.set()
+            await callback_query.message.edit_text('На какое время хотите забронировать столик?\n'
+                                                'Время вводится в формате Часы:Минуты')
 
 
-def is_valid_time(message_time) -> bool:
+async def is_valid_time(message_time) -> bool:
     message_time = message_time['text'].split(':')
     if 24 > int(message_time[0]) > 00 and 60 >= int(message_time[1]) >= 0:
         return True
@@ -85,20 +87,24 @@ def is_valid_time(message_time) -> bool:
 
 @dp.message_handler(state=BookingStates.time)
 async def booking_time(message: types.Message, state=FSMContext):
-    if not is_valid_time(message):
+    if not await is_valid_time(message):
+        await message.delete()
         await message.answer("Время введено не корректно, проверьте правильность ввода.\n"
                              "Формат ввода Часы:Минуты")
     else:
-        await message.answer('Время введено корректно и записано.')
+
         await state.update_data(time=message.text)
         await BookingStates.next()
         await BookingStates.num_of_people.set()
-        await message.answer('Введите количество человек')
+        await message.delete()
+        await message.answer('Время введено корректно и записано.\n'
+                             'Введите количество человек')
 
 
 @dp.message_handler(state=BookingStates.num_of_people)
 async def booking_peoples(message: types.Message, state=FSMContext):
     if 0 > int(message.text) > 6:
+        await message.delete()
         await message.answer('Количество человек должно быть не больше 6 за один стол.')
     else:
         await state.update_data(num_of_people=message.text)
@@ -108,6 +114,7 @@ async def booking_peoples(message: types.Message, state=FSMContext):
         date = booking_data['date']
         time = booking_data['time']
         number_people = booking_data['num_of_people']
+        await message.delete()
         await message.answer(f'Столик успешно забронирован на имя {f_name} {l_name}.\nДата: {date}, время: {time}.\n'
                              f'Количество человек: {number_people}.\n'
                              f'Для возвращения в главное меню, нажмите "Назад".',
